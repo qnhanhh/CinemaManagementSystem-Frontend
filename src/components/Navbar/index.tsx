3af3aa1ea524621a12032cf8820c567eb96ef5a4
qnhanhh <caoquynhanh18@gmail.com>
2023-06-26 00:17:33 +0700

    feat/project base

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import Logo from "./logo";
+import Menu from "./menu";
+
+export default function Navbar(){
+    return(
+        <div>
+            <Logo />
+            <Menu />
+        </div>
+    )
+}
---
+++ src/components/Navbar/logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <div className="text-white font-bold uppercase text-3xl tracking-widest">
+      <h1>Muuvii</h1>
+    </div>
+  );
+}

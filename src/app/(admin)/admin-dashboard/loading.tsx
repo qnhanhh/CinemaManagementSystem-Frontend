f6ef7eaa0c2048607a1f3fbcda08a5f6007da676
qnhanhh <caoquynhanh18@gmail.com>
2023-07-01 12:24:51 +0700

    add admin movies task table

---
+++ src/app/(admin)/admin-dashboard/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingButton from "@/components/loading-button";
+
+export default function Loading() {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <LoadingButton />
+    </div>
+  );
+}
